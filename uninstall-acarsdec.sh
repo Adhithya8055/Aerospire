966f1668440e6704dd923b273c96a0e83d52b4b6
Adhithya Pasupuleti <92710784+Adhithya8055@users.noreply.github.com>
2023-01-13 00:02:23 +0530

    Create uninstall-acarsdec.sh

---
+++ uninstall-acarsdec.sh
@@ -0,0 +1,17 @@
+# Uninstallation
+echo "Uninstalling Acarsdec..."
+
+# Remove RTL-SDR
+sudo make uninstall -C /usr/src/rtl-sdr/build
+sudo rm -rf /usr/src/rtl-sdr
+sudo apt-get remove cmake git libusb-1.0 libusb-1.0-dev pkg-config rtl-sdr gr-osmosdr -y
+
+# Remove libacars-package
+sudo make uninstall -C /usr/src/libacars/build
+sudo rm -rf /usr/src/libacars
+
+# Remove acarsdec
+sudo make uninstall -C /usr/src/acarsdec/build
+sudo rm -rf /usr/src/acarsdec
+
+echo "Acarsdec has been uninstalled."
