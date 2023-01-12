f74476a65e2c1bbed54063a2ee0ef38b373524c7
Adhithya Pasupuleti <92710784+Adhithya8055@users.noreply.github.com>
2023-01-12 21:16:43 +0530

    Update install-acarsdec.sh

--- install-acarsdec.sh
+++ install-acarsdec.sh
@@ -1,71 +1,60 @@
+#!/bin/bash
+
 #About
 clear
-echo "\033[1;36m Acars complete installation on kali linux for RTL-SDR "
+echo "\033[1;36m Acars complete installation on Raspbian, Kali Linux, and Ubuntu "
 echo '--------------------------------------------------------------'
 sleep 3
 echo "\033[1;33m disclaimer: The following installation packages & repositories in this installation belongs to their respective owners." 
 sleep 4
 
-
-#updating packages
+# Update package list
 sudo apt update
-clear
 
-
-#Installing RTL-driver
+# Install RTL-driver
 echo "\033[0;32m installing rtl-driver"
-sudo apt-get install cmake git libusb-1.0 libusb-1.0-0.dev pkg-config rtl-sdr gr-osmosdr -y
-clear
-git clone https://github.com/osmocom/rtl-sdr.git
-cd /home/kali/rtl-sdr/
-mkdir build
-cd build/
-sleep 2
+if ! command -v apt-get > /dev/null; then
+    sudo apt install cmake git libusb-1.0 libusb-1.0-dev pkg-config rtl-sdr gr-osmosdr -y
+else
+    sudo apt-get install cmake git libusb-1.0 libusb-1.0-dev pkg-config rtl-sdr gr-osmosdr -y
+fi
+
+# Clone the repository for RTL-SDR
+sudo mkdir -p /usr/src/rtl-sdr
+sudo git clone https://github.com/osmocom/rtl-sdr.git /usr/src/rtl-sdr
+
+# Build and install RTL-SDR
+cd /usr/src/rtl-sdr
+sudo mkdir build
+cd build
 sudo cmake ../ -DDETACH_KERNEL_DRIVER=ON -DINSTALL_UDEV_RULES=ON
-sleep 2
-clear
 sudo make
-clear
-sleep 2
 sudo make install
-sleep 2
 sudo ldconfig
 
-
-#Installing libacars-package
-clear
+# Install libacars-package
 echo "\033[0;32m installing libacars-package"
-cd /home/kali/
-git clone https://github.com/szpajder/libacars.git
-cd /home/kali/libacars
-mkdir build
+sudo mkdir -p /usr/src/libacars
+sudo git clone https://github.com/szpajder/libacars.git /usr/src/libacars
+cd /usr/src/libacars
+sudo mkdir build
 cd build
 sudo cmake ..
-sleep 2
 sudo make
-sleep 2
 sudo make install
 
-
-#Installing acarsdec
-clear
-cd /home/kali/
+# Install acarsdec
 echo "\033[0;32m installing acarsdec"
-git clone https://github.com/TLeconte/acarsdec.git
-cd /home/kali/acarsdec
-mkdir build
-cd build/
-sleep 4
+sudo mkdir -p /usr/src/acarsdec
+sudo git clone https://github.com/TLeconte/acarsdec.git /usr/src/acarsdec
+cd /usr/src/acarsdec
+sudo mkdir build
+cd build
 sudo cmake .. -Drtl=ON
-clear
-sleep 4
 sudo make
-clear
-sleep 4
 sudo make install
-sleep 4
-ldconfig
+sudo ldconfig
 
-
-#Instruction
+# Instruction
 echo "\033[0;36m Find the instruction on how to run acarsdec in README.md"
+
